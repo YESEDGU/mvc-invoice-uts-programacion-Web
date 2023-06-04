55037ec2fc31c8598fcd49dc2aa0cccb89337e61
Yesid <Yesid@Asus>
2023-06-03 19:11:46 -0500

    Primer Push

---
+++ src/main/java/com/uts/invoice/programacion/web/models/entities/Producto.java
@@ -0,0 +1,74 @@
+package com.uts.invoice.programacion.web.models.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name="productos")
+public class Producto implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty(message="El nombre no puede estar vacio")
+	@Column(nullable=true)
+	private String nombre;
+	
+	
+	private Double precio;
+	
+	@NotNull
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createAt;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createAt=new Date();		
+	}	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+}
