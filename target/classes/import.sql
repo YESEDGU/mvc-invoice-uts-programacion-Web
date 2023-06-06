58f29abe12a63881d74c16a2283b887db614f855
Yesid <yesideduardoguerrero@hotmail.com>
2023-06-06 04:03:06 -0500

    push, actualizacion base de datos

---
+++ target/classes/import.sql
@@ -0,0 +1,11 @@
+/* Populate Tables */
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Camilo', 'Rodriguez', 'profesor@gmail.com', '2023-04-01');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Yesid', 'Guerrero', 'yesid.guerrero@gmail.com', '2023-04-02');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Angie', 'Sarmiento', '	a.sarmiento@hotmail.com', '2023-04-03');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Jose Luis', 'Mantilla', 'jose.mantilla@gmail.com', '2023-04-04');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Diego Fernando', 'Mantilla', 'dfm@gmail.com', '2023-04-05');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Maria', 'Lopez', 'maria.lopezr@yahoo.com', '2023-04-06');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Tatiana', 'Martinez', 'tatiana0322@gmail.com', '2023-04-07');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Jonatha', 'Davis', 'jdavis86@gmail.com', '2023-04-08');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Diana', 'Calderon', 'dcalderon@gmail.com', '2023-04-09');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Marly', 'Santander', 'marly.santander@gmail.com', '2023-04-10');
